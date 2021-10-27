e19522f9eebb4d544ad04a76cffbd3b19a862eae
yorkane <whyork@qq.com>
2021-10-27 21:34:33 +0800

    add  croaring lib build

--- deps.sh
+++ deps.sh
@@ -1,13 +1,5 @@
 #!/usr/bin/env bash
-# wait for update...
-
 # libroaring building:
-#https://github.com/lemire/CRoaringUnityBuild
-# version='0.3.4'
-# rm -rf src/roaring
-# echo https://github.com/RoaringBitmap/CRoaring/releases/download/v0.4.0/roaring.zip
-# echo https://github.com/lemire/CRoaringUnityBuild/archive/v${version}.tar.gz
-# curl https://github.com/lemire/CRoaringUnityBuild/archive/v${version}.tar.gz -Lk | tar -xvz -C src/
-# mv src/CRoar* src/roaring
-
-curl -L https://raw.githubusercontent.com/RedisLabsModules/RedisModulesSDK/master/redismodule.h -o src/redismodule.h
+curl -Lk  https://github.com/RoaringBitmap/CRoaring/releases/download/v0.4.0/roaring.zip | bsdtar -xkf- -C croaring
+cd croaring
+make && make install
